6ff20b2f52d78ea56e82d4c8ce820cf26bd8eff1
Steph <1000p@mail.ru>
2022-04-28 17:06:38 +0700

    + 115

--- 115.py
+++ 115.py
@@ -1,2 +1,25 @@
 # Строка содержит набор чисел. Показать большее и меньшее число
 # Символ-разделитель - пробел
+
+
+str = '30 3 6 455 23 7 8 3 21 64 4'
+
+str = list(str.split(' '))
+str = list(map(int,str))
+
+min = str[0]
+max = str[0]
+
+for i in range(len(str)):
+    if str[i] > max :
+        max = str[i]
+    if str[i] < min :
+        min = str[i]
+
+
+print(max)
+print(min)
+
+
+
+print(f'Максимальное число: {max}  Минимальное число  {min}')
