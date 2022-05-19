e4063ec181c9f74eab8f07770034df5063df98e7
Steph <1000p@mail.ru>
2022-05-19 11:41:02 +0700

    test006

--- 206.py
+++ 206.py
@@ -1,1 +1,19 @@
 # Напишите программу, удаляющую из текста все слова содержащие "абв".
+
+text = 'п укдлпукпуклмидьук укапкуп куа кп кабв уакабвабвабв абв  енуеабв  акупукрбв  абууубав  ваб'
+
+
+# text= text.split()
+
+# def delT(tex,x):
+#     list =[]
+#     for i in range(len(tex)):
+#         if x not in tex[i]:
+#              list.append(tex[i])
+#     return list
+
+# print(text:=delT(text,'абв'))
+
+string = list(filter(lambda x: 'абв' not in x, text.split()))
+result = " ".join(string)
+print(result)
--- 210.py
+++ 210.py
@@ -1,18 +1,18 @@
 # Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
 # входные и выходные данные хранятся в отдельных текстовых файлах
 
+data = 'wwweregerrrrg444443grdf3ggggggggggggrerg33333333eeqqq'
+newdata = ''
+first = ''
+count = 1
 
-start = [1, 2, 3, 5, 1,1, 5, 3, 10]
-end = []
-
-for i in range(len(start)):
-    count=0
-    for q in range(i+1,len(start)):
-        if start[i] == start[q]:
-            print (i,q)
-            count+=1
-            i=+1
-            if count<2:
-                end.append(start[i])
-
-print(end)
+for i in data:
+    if i != first:
+        if first != '':
+            newdata += str(count)+first
+        count = 1
+        first = i
+    else:
+        count += 1
+newdata += str(count)+data[-1]
+print(newdata)
