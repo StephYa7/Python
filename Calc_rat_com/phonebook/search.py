2d61471b6606f64781fd0a6be3fdbf4b32bcc98d
Steph <1000p@mail.ru>
2022-05-24 10:27:40 +0700

    search ++

--- Calc_rat_com/phonebook/search.py
+++ Calc_rat_com/phonebook/search.py
@@ -7,25 +7,31 @@ def search(line):
     if line == 1:
         name = input('Введите Фамилию: ')
         for line in data:
-            line = line.split(',')
-            if line[0] == name:
+            sline = line.split(',')
+            if sline[0] == name:
                 print(line)
-    data.close()
     if line == 2:
         name = input('Введите Имя: ')
         for line in data:
-            line = line.split(',')
-            if line[1] == name:
+            sline = line.split(',')
+            if sline[1] == name:
                 print(line)
-    data.close()
     if line == 3:
         name = input('Введите Номер телефона: ')
         for line in data:
-            line = line.split(',')
-            if line[2] == name:
+            sline = line.split(',')
+            if sline[2] == name:
                 print(line)
+    if line == 4:
+        name = input('Введите слово из заметки: ')
+        for line in data:
+            sline = line.split(',')
+            target = sline[3].split()
+            for i in target:
+                if i.lower() == name.lower():
+                    print(line)
     data.close()
 
+    
 
-
-print(search(1))
+print(search(4))
--- Calc_rat_com/phonebook/test.py
+++ Calc_rat_com/phonebook/test.py
@@ -1,14 +1,6 @@
-import io
+a = 'dweqfEFWFWEfewFWEfwefewf'
 
-path = 'Calc_rat_com\phonebook\Phone_book_line.xls'            # Загрузка
-data = io.open(path, 'r',encoding='utf-8')
-for line in data:
-    print(line)
-data.close()
-
-
-
-
+print(a.lower)
 
 
 
